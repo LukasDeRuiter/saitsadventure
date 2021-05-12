7c743aa4a1fe1ea84ad2117988fa3abb148ba4d7
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-05-12 15:05:19 +0200

    added sait spritsheet and walking animation

--- level1.js
+++ level1.js
@@ -5,7 +5,7 @@ class Level1 extends Phaser.Scene {
 
 
     preload(){
-        this.load.image('sait', 'assets/sait.png');
+        this.load.spritesheet( 'sait', 'assets/saitSpritesheet.png', { frameWidth: 64, frameHeight: 64 });
         this.load.image('platform', 'https://content.codecademy.com/courses/learn-phaser/physics/platform.png');
     }
 
@@ -13,6 +13,22 @@ class Level1 extends Phaser.Scene {
     create(){
         this.sait = this.physics.add.sprite(400, 300, 'sait').setScale(2);
         this.sait.setCollideWorldBounds(true);
+        this.anims.create({
+            key: 'run',
+            frames:
+            this.anims.generateFrameNumbers('sait', {start: 1, end: 8}),
+            frameRate: 10,
+            repeat: -1
+        } 
+    )
+    this.anims.create({
+        key: 'idle',
+        frames:
+        this.anims.generateFrameNumbers('sait', {start: 0, end: 0}),
+        frameRate: 10,
+        repeat: -1  
+    })
+
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(320, 550, 'platform');
         this.physics.add.collider(this.sait, this.platforms);
@@ -23,12 +39,17 @@ class Level1 extends Phaser.Scene {
     update(){
         if(this.cursors.left.isDown) {
             this.sait.setVelocityX(-160);
+            this.sait.anims.play('run', true);
+            this.sait.flipX = true;
         }
         else if(this.cursors.right.isDown){
             this.sait.setVelocityX(160);
+            this.sait.anims.play('run', true);
+            this.sait.flipX = false;
         }
         else{
             this.sait.setVelocityX(0);
+            this.sait.anims.play('idle', true);
         }
 
     }
