90957a855b666745f70b1707b29e8c7cd42c7dfc
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-05-18 23:24:21 +0200

    added more fixes

--- gameHud.js
+++ gameHud.js
@@ -11,33 +11,43 @@ class GameHud extends Phaser.Scene {
     }
 
     create(){
-        this.hearts = this.add.group({
-            classType: Phaser.GameObjects.Image})
 
-            this.hearts.createMultiple({
-                key: 'fullHeart',
-                setXY: {
-                    x: 10,
-                    y: 10,
-                    stepX: 32
-                },
-                quantity: 3
+        this.hearts = this.add.group();
+        for(let i = 0; i < 3; i++){
+            this.hearts.create((40 + (40 * i)), 40, 'fullHeart');
 
-            })
+        }
+        this.heartNum = 1;
+        this.hearts.getChildren().forEach((heart) => {
+
+            heart.name = `heart${this.heartNum}`;
+            this.heartNum += 1;
+            console.log(heart);
+        })
+   
+
             this.ourGame = this.scene.get('Level1');
             this.ourGame.events.on('damage', changeHealth, this.SaitHealth);
 
+        }} 
+
+        
             function changeHealth(health){
                 if(health === 3){
-                    this.hearts.children[0].setTexture('fullHeart');
-                    this.hearts.children[1].setTexture('fullHeart');
-                    this.hearts.children[2].setTexture('fullHeart');
+                    this.hearts.getChildren().forEach((heart) => {
+                        heart.setTexture('fullHeart');
+                    })
+               
                 }
                 else if(health === 2){
-                     this.hearts.children[0].setTexture('fullHeart');
-                     this.hearts.children[1].setTexture('fullHeart');
-                     this.hearts.children[2].setTexture('emptyHeart');
-                }
+                    this.hearts.getChildren().forEach((heart) => {
+                        heart.setTexture('fullHeart');
+                        if(heart.name = "heart3"){
+                            heart.setTexture('emptyHeart');
+                        }
+                    })
+                }}
+                /*
                 else if(health === 1){
                     this.hearts.children[0].setTexture('fullHeart');
                     this.hearts.children[1].setTexture('emptyHeart');
@@ -48,6 +58,7 @@ class GameHud extends Phaser.Scene {
                     this.hearts.children[1].setTexture('emptyHeart');
                     this.hearts.children[2].setTexture('emptyHeart');
                 }
-            }
+            }  
+*/
 
-    }}
+ 
